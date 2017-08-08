4437dfa571629af000f122059859e13cbe03bbb1
Pastrana, Rodrigo <rodrigo.pastrana@lexisnexis.com>
2017-08-08 11:33:11 -0400

    JAPI-85 Platform tester update
    - Creates and uploads test data
    - Test cases based on available data
    - Streamlines tests
    - Accepts arguments for urls and credentials
    - Conditionally executes WsSQL cases
    - Prompts upon test finish
    
    Signed-off-by: Pastrana, Rodrigo <rodrigo.pastrana@lexisnexis.com>

--- wsclient/src/test/java/org/hpccsystems/ws/client/platform/test/PlatformTester.java
+++ wsclient/src/test/java/org/hpccsystems/ws/client/platform/test/PlatformTester.java
@@ -1,8 +1,14 @@
 package org.hpccsystems.ws.client.platform.test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 import org.hpccsystems.ws.client.HPCCWsClient;
 import org.hpccsystems.ws.client.HPCCWsDFUClient;
@@ -17,17 +23,156 @@ import org.hpccsystems.ws.client.platform.EclRecordInfo;
 import org.hpccsystems.ws.client.platform.Platform;
 import org.hpccsystems.ws.client.platform.Version;
 import org.hpccsystems.ws.client.platform.WorkunitInfo;
+import org.hpccsystems.ws.client.platform.test.data.Accounts;
+import org.hpccsystems.ws.client.platform.test.data.Persons;
+import org.hpccsystems.ws.client.utils.Connection;
 import org.hpccsystems.ws.client.utils.FileFormat;
 import org.hpccsystems.ws.client.utils.Utils;
 
 public class PlatformTester
 {
+    public static void writeFile(String filename, String content, boolean overwrite)
+    {
+        BufferedWriter output = null;
+        try
+        {
+            File file = new File(filename);
+            if (!file.exists() || overwrite)
+            {
+                output = new BufferedWriter(new FileWriter(file));
+                output.write(content);
+            }
+            else
+                System.out.println(filename + " exists! Will NOT overwrite!");
+        }
+        catch ( IOException e )
+        {
+            System.out.println("Could not create and populate file (" + filename + ")!");
+            System.out.println(e.getLocalizedMessage());
+        }
+        finally
+        {
+          if ( output != null )
+          {
+            try
+            {
+                output.close();
+            }
+            catch (IOException e)
+            {
+                System.out.println("Could not close newly created file (" + filename + ")!");
+                System.out.println(e.getLocalizedMessage());
+            }
+          }
+        }
+    }
+
+    private static void usage()
+    {
+        System.out.println("Options: "
+                + "-server=<HPCCServerAddress>          DEFAULT:localhost"
+                + "-port=<ECLWatchPort                  DEFAULT:8010"
+                + "-protocol=<ECLWatchAddressProtocol>  DEFAULT:http"
+                + "-user=<ECLWatch User>                DEFAULT:"
+                + "-pass=<ECLWatch Pass>                DEFAULT:"
+                + "-machineuser=<machine's username>    DEFAULT:"
+                + "-machinepass=<machine's password>    DEFAULT:"
+                + "-wssqlport=<WsSQLPort>               DEFAULT:8510");
+
+        System.exit(-1);
+    }
+
+    static private final String PARAMPREFIX          = "-(?i)";
+    static public final String  SERVERPATTERN    = PARAMPREFIX + "server";
+    static public final String  PORTPATTERN    = PARAMPREFIX + "port";
+    static public final String  PROTOCOLPATTERN    = PARAMPREFIX + "protocol";
+    static public final String  USERPATTERN    = PARAMPREFIX + "user";
+    static public final String  PASSPATTERN    = PARAMPREFIX + "pass";
+    static public final String  MACHINEUSERPATTERN    = PARAMPREFIX + "machineuser";
+    static public final String  MACHINEPASSPATTERN    = PARAMPREFIX + "machinepass";
+    static public final String  WSSQLPORTPATTERN    = PARAMPREFIX + "wssqlport";
 
     public static void main(String[] args)
     {
+        String hpccServer = "localhost";
+        int port = 8010;
+        String prot = "http";
+        String user = "";
+        String pass = "";
+        String machineuser = "";
+        String machinepass = "";
+        String wssqlport = "8510";
+
+        for (int i = 0; i < args.length; i++)
+        {
+            StringTokenizer params = new StringTokenizer(args[i], "=");
+
+            while (params.hasMoreElements())
+            {
+                String currentParam = (String) params.nextElement();
+                String currentParamVal = null;
+
+                if (params.hasMoreElements())
+                {
+                    currentParamVal = (String) params.nextElement();
+                }
+                else
+                {
+                    Utils.println(System.out, "ERROR: " + currentParam + "=??", false, false);
+                    usage();
+                }
+                if (currentParam.matches(SERVERPATTERN))
+                {
+                    hpccServer = currentParamVal;
+                    Utils.println(System.out, "TARGET HPCC SERVER: " + currentParamVal, false, false);
+                }
+                else if (currentParam.matches(PORTPATTERN))
+                {
+                    port = Integer.parseInt(currentParamVal);
+                    Utils.println(System.out, "TARGET HPCC PORT: " + currentParamVal, false, false);
+                }
+                else if (currentParam.matches(PROTOCOLPATTERN))
+                {
+                    prot = currentParamVal;
+                    Utils.println(System.out, "TARGET HPCC PROTOCOL: " + currentParamVal, false, false);
+                }
+                else if (currentParam.matches(USERPATTERN))
+                {
+                    user = currentParamVal;
+                    Utils.println(System.out, "TARGET HPCC User: " + currentParamVal, false, false);
+                }
+                else if (currentParam.matches(USERPATTERN))
+                {
+                    pass = currentParamVal;
+                    Utils.println(System.out, "TARGET HPCC password set!", false, false);
+                }
+                else if (currentParam.matches(MACHINEUSERPATTERN))
+                {
+                    machineuser = currentParamVal;
+                    Utils.println(System.out, "TARGET machine user: " + currentParamVal, false, false);
+                }
+                else if (currentParam.matches(MACHINEPASSPATTERN))
+                {
+                    machinepass = currentParamVal;
+                    Utils.println(System.out, "TARGET machine password set!", false, false);
+                }
+                else if (currentParam.matches(WSSQLPORTPATTERN))
+                {
+                    wssqlport = currentParamVal;
+                    Utils.println(System.out, "TARGET WsSQL port: " + currentParamVal, false, false);
+                }
+
+                else
+                {
+                    Utils.println(System.out, "Unrecognized param: " + currentParam, false, false);
+                    usage();
+                }
+            }
+        }
+
         try
         {
-            Platform platform = Platform.get("http", "192.168.56.120", 8010, "", "");
+            Platform platform = Platform.get(prot, hpccServer, port, user, pass);
 
             Version v = platform.getVersion();
             System.out.println(v.toString());
@@ -59,10 +204,11 @@ public class PlatformTester
             platform.checkInHPCCWsClient(client3);
             platform.checkInHPCCWsClient(client4);
 
-            HPCCWsSQLClient wsSQLClient = platform.getWsClient().getWsSQLClient("8510");
-            String s = "CREATE TABLE newtablename (id DATE, mytint INT(9), mydouble DOUBLE (5,3) UNSIGNED) LOAD DATA INFILE 'OriginalPerson' CONNECTION '10.0.2.15' DIRECTORY '/var/lib/HPCCSystems/mydropzone' INTO TABLE newtablename";
-            ExecuteSQLResponse executeSQLFullResponse = wsSQLClient.executeSQLFullResponse(s, "thor", "thor", Integer.valueOf(0), Integer.valueOf(0),Integer.valueOf(0), false, false, "me", Integer.valueOf(-1));
-            System.out.println(executeSQLFullResponse.getResult());
+            String tmpPeopleFile = System.getProperty("java.io.tmpdir") + File.separator + "people";
+            writeFile( tmpPeopleFile, Persons.data, false);
+
+            String tmpAccountFile = System.getProperty("java.io.tmpdir") + File.separator + "account";
+            writeFile( tmpAccountFile, Accounts.data, false);
 
             int pport = HPCCWsAttributesClient.getOriginalPort();
             HPCCWsClient connector = platform.checkOutHPCCWsClient();
@@ -71,31 +217,12 @@ public class PlatformTester
             HPCCWsDFUClient wsDFUClient = connector.getWsDFUClient();
             platform.checkInHPCCWsClient(connector);
 
-            EclRecordInfo datasetFields = wsDFUClient.getDatasetFields("lotto::winning::numbers::date::csv", null, "," );
-            HashMap<String,DFURecordDefInfo> recordsets = datasetFields.getRecordsets();
-            Set<String> keySet = recordsets.keySet();
-            for (String field : keySet)
-            {
-                DFURecordDefInfo dfuRecordDefInfo = recordsets.get(field);
-                List<DFUDataColumnInfo> childColumns = dfuRecordDefInfo.getChildColumns();
-                for (DFUDataColumnInfo dfuDataColumnInfo : childColumns)
-                {
-                    System.out.println(dfuDataColumnInfo.getColumnEclType() + " " + dfuDataColumnInfo.getColumnLabel());
-                }
-            }
-
-            DFUDataColumn[] newgetFileDataColumns = wsDFUClient.getFileMetaData("kevinsfile::lotto_0215::processed.csv", null);
-            for (int i = 0; i < newgetFileDataColumns.length; i++)
-            {
-                System.out.println("Col name: " + newgetFileDataColumns[i].getColumnLabel() + " ecl: " + newgetFileDataColumns[i].getColumnEclType() + " col type " + newgetFileDataColumns[i].getColumnType());
-            }
             connector = platform.checkOutHPCCWsClient();
             System.out.println("wsfileio ver: " + connector.getWsFileIOClientVer());
             System.out.println("wssmc ver: " + connector.getWsSMCClientClientVer());
-            //connector.uploadFileToHPCC(new File("C://assignments//data//shortpersons"));
-            //connector.uploadFileToHPCC(new File("C://assignments//data//shortaccounts"));
-            //connector.uploadFileToHPCC(new File("C://assignments//data//small.txt"));
-            connector.uploadFileToHPCC("C://assignments//data//shortpersons", "myhpcctargetname", "hadoop", "hadoop");
+
+            connector.uploadFileToHPCC(tmpPeopleFile, "people-small", machineuser, machinepass );
+            connector.uploadFileToHPCC(tmpAccountFile, "account-small", machineuser, machinepass);
 
             //for our curiosity, what cluster groups and clusters are available.
             List<String> clusters = connector.getAvailableTargetClusterNames();
@@ -114,70 +241,64 @@ public class PlatformTester
             }
 
             //pick one of the valid clusters
-            connector.sprayFlatHPCCFile("shortpersons", "mythor::shortpersons", 155, clusters.get(0) /*myroxie, mythor, etc*/, true);
+            connector.sprayFlatHPCCFile("people-small", "mythor::persons",Persons.recLen, clusters.get(0) /*myroxie, mythor, etc*/, true);
 
-            System.out.println(FileFormat.getFileFormatName(Utils.findEnumValFromString(FileFormat.class, "CSV")));
-            connector.sprayDefaultCSVHPCCFile("shortaccounts","mythor::shortaccounts", clusters.get(0), true);
 
-            String outputCSVfileContentsEcl = "Layout_CSV_Accounts := RECORD" +
-                    "    UNSIGNED INTEGER8 PersonID;" +
-                    "    STRING8 ReportDate;" +
-                    "    STRING2 IndustryCode;" +
-                    "    UNSIGNED4 Member;" +
-                    "    STRING8 OpenDate;" +
-                    "    STRING1 TradeType;" +
-                    "    STRING1 TradeRate;" +
-                    "    UNSIGNED1 Narr1;" +
-                    "    UNSIGNED1 Narr2;" +
-                    "    UNSIGNED4 HighCredit;" +
-                    "    UNSIGNED4 Balance;" +
-                    "    UNSIGNED2 Terms;" +
-                    "    UNSIGNED1 TermTypeR;" +
-                    "    STRING15  AccountNumber;" +
-                    "    STRING8 LastActivityDate;" +
-                    "    UNSIGNED1 Late30Day;" +
-                    "    UNSIGNED1 Late60Day;" +
-                    "    UNSIGNED1 Late90Day;" +
-                    "    STRING1 TermType;" +
-                    "END;" +
-                    "" +
-                    "hpccaccountrecords := DATASET('~mythor::shortaccounts', Layout_CSV_Accounts, CSV);" +
-                    "output(hpccaccountrecords) ;";
-
-            String outputFlatfileContentsEcl = "Layout_Persons := RECORD " +
-                    "UNSIGNED INTEGER8 ID;" +
-                    "STRING15 FirstName;" +
-                    "STRING25 LastName;" +
-                    "STRING15 MiddleName;" +
-                    "STRING2 NameSuffix;" +
-                    "STRING8 FileDate;" +
-                    "UNSIGNED INTEGER2 BureauCode;" +
-                    "STRING1  MaritalStatus;" +
-                    "STRING1 Gender;" +
-                    "UNSIGNED INTEGER1 DependentCount;" +
-                    "STRING8 BirthDate;" +
-                    "STRING42 StreetAddress;" +
-                    "STRING20 City;" +
-                    "STRING2 State;" +
-                    "STRING5 ZipCode;" +
-                    "END;" +
-                    "" +
-                    "hpccpersonrecords := DATASET('~mythor::shortpersons', Layout_Persons, FLAT);" +
-                    "output(hpccpersonrecords) ;";
-
+            String outputFlatfileContentsEcl = Persons.elcLayout +
+                    "hpccpersonrecords := DATASET('~mythor::persons', Layout_Persons, FLAT);" +
+                   "OUTPUT(hpccpersonrecords,, 'processed::persons',OVERWRITE);";
 
             WorkunitInfo wu=new WorkunitInfo();
-            wu.setECL(outputCSVfileContentsEcl);
-            wu.setJobname("myflatoutput");
+            wu.setECL(outputFlatfileContentsEcl);
+            wu.setJobname("mypersonsfile");
             wu.setCluster(clusterGroups[0]);
-            wu.setResultLimit(100);
+            wu.setResultLimit(10);
             wu.setMaxMonitorMillis(50000);
-            wu.setJobname("myjobname");
             //this is just one way to submitECL, you can also submit via ecldirect and request the resulting WUID
             //you can also, submit via WSWorkunits and have more control over the result window you get back.
             String results = connector.submitECLandGetResults(wu);
 
-            //List<List <Object>> resultsList = Utils.parseECLResults(results);
+            System.out.println("Persons file content(10 recs) output: " + results);
+
+            EclRecordInfo datasetFields = wsDFUClient.getDatasetFields("hthor::processed::persons", null, "," );
+
+            HashMap<String,DFURecordDefInfo> recordsets = datasetFields.getRecordsets();
+            Set<String> keySet = recordsets.keySet();
+            for (String field : keySet)
+            {
+                DFURecordDefInfo dfuRecordDefInfo = recordsets.get(field);
+                List<DFUDataColumnInfo> childColumns = dfuRecordDefInfo.getChildColumns();
+                for (DFUDataColumnInfo dfuDataColumnInfo : childColumns)
+                {
+                    System.out.println(dfuDataColumnInfo.getColumnEclType() + " " + dfuDataColumnInfo.getColumnLabel());
+                }
+            }
+
+            DFUDataColumn[] newgetFileDataColumns = wsDFUClient.getFileMetaData("hthor::processed::persons", null);
+            for (int i = 0; i < newgetFileDataColumns.length; i++)
+            {
+                System.out.println("Col name: " + newgetFileDataColumns[i].getColumnLabel() + " ecl: " + newgetFileDataColumns[i].getColumnEclType() + " col type " + newgetFileDataColumns[i].getColumnType());
+            }
+
+            System.out.println(FileFormat.getFileFormatName(Utils.findEnumValFromString(FileFormat.class, "CSV")));
+            connector.sprayDefaultCSVHPCCFile("account-small","mythor::accounts", clusters.get(0), true);
+
+            String outputCSVfileContentsEcl = Accounts.elcLayout +
+                    "hpccaccountrecords := DATASET('~mythor::accounts', Layout_Accounts, CSV);" +
+                   "OUTPUT(hpccaccountrecords,, 'processed::accounts',OVERWRITE);";
+
+            wu=new WorkunitInfo();
+            wu.setECL(outputCSVfileContentsEcl);
+            wu.setJobname("myaccountsfile");
+            wu.setCluster(clusterGroups[0]);
+            wu.setResultLimit(10);
+            wu.setMaxMonitorMillis(50000);
+            //this is just one way to submitECL, you can also submit via ecldirect and request the resulting WUID
+            //you can also, submit via WSWorkunits and have more control over the result window you get back.
+            results = connector.submitECLandGetResults(wu);
+
+            System.out.println("Accounts file content(10 recs) output: " + results);
+
             wu.setCluster(clusterGroups[0]); /*hthor,thor,roxie*/
             WUPublishWorkunitResponse publishresp = connector.getWsWorkunitsClient().publishWUFromEcl(wu);
 
@@ -207,10 +328,37 @@ public class PlatformTester
             String wuid = connector.getWsWorkunitsClient().createAndRunWUFromECLAndGetWUID(wu);
             String results2 = connector.getWsWorkunitsClient().fetchResults(wuid, 0, clusterGroups[1], true, -1, -1);
             System.out.println(results2);
+
+
+            try
+            {
+                //WSSQL Test
+                HPCCWsSQLClient wsSQLClient = platform.getWsClient().getWsSQLClient(wssqlport);
+                String s = "CREATE TABLE newtablename (FirstName VARCHAR(15), LastName VARCHAR(25), MiddleName VARCHAR(15) ,StreetAddress VARCHAR(42), city VARCHAR(20), state VARCHAR(2), zip VARCHAR(5)) LOAD DATA INFILE 'people-small' CONNECTION '10.0.2.15' DIRECTORY '/var/lib/HPCCSystems/mydropzone' INTO TABLE newtablename";
+                ExecuteSQLResponse executeSQLFullResponse = wsSQLClient.executeSQLFullResponse(s, "thor", "thor", Integer.valueOf(0), Integer.valueOf(0),Integer.valueOf(0), false, false, "me", Integer.valueOf(-1));
+                System.out.println(executeSQLFullResponse.getResult());
+
+                s = "SELECT * from newtablename  where state = 'FL' limit 10;";
+                executeSQLFullResponse = wsSQLClient.executeSQLFullResponse(s, "thor", "thor", Integer.valueOf(0), Integer.valueOf(0),Integer.valueOf(0), false, false, "me", Integer.valueOf(-1));
+                System.out.println(executeSQLFullResponse.getResult());
+            }
+            catch (java.net.ConnectException e)
+            {
+                System.out.println("Could not connect to WsSQL on port: " + wssqlport + "\n>>" + e.getLocalizedMessage());
+            }
+            catch (Exception e)
+            {
+                System.out.println("Encountered issue while testing WsSQL on port: " + wssqlport + "\n>>" + e.getLocalizedMessage());
+            }
+
         }
         catch (Exception e)
         {
             System.out.println( e.getLocalizedMessage());
+        }
+        finally
+        {
+            System.out.println("\n****WsClient HPCC platform tester has finished****\n" );
         }
 
     }
