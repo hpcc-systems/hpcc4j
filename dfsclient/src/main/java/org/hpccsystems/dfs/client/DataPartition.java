6f48bbce379c64627655259ffd8b0fecf32c0508
James McMullan <jpmcmu@gmail.com>
2019-01-15 16:15:10 -0500

    JAPI-138 Created DFS Client sub project (#230)
    * JAPI-138 Introduce DAFILESRV client subproject
    
    - Reading side changes
    
    - Writing side changes
    
    - Added remote read testing
    
    Signed-off-by: James McMullan <James.McMullan@lexisnexis.com>

---
+++ dfsclient/src/main/java/org/hpccsystems/dfs/client/DataPartition.java
@@ -0,0 +1,232 @@
+/*******************************************************************************
+ *     HPCC SYSTEMS software Copyright (C) 2018 HPCC Systems®.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *******************************************************************************/
+package org.hpccsystems.dfs.client;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import org.hpccsystems.commons.ecl.FileFilter;
+import org.hpccsystems.commons.errors.HpccFileException;
+import org.hpccsystems.dfs.cluster.ClusterRemapper;
+import org.hpccsystems.ws.client.platform.DFUFilePartInfo;
+import org.hpccsystems.ws.client.wrappers.wsdfu.DFUFilePartWrapper;
+
+/**
+ * A partition of data.  One physical file
+ * or key accessed by HPCC remote read.
+ */
+public class DataPartition implements Serializable
+{
+    public static final long serialVersionUID = 1L;
+    private String[]         copyLocations;
+    private String           file_name;
+    private int              this_part;
+    private int              num_parts;
+    private int              rowservicePort;
+    private boolean          useSSL;
+    private FileFilter       fileFilter;
+    private String           fileAccessBlob;
+
+    /**
+     * Construct the data part, used by makeParts
+     * @param copylocations locations of all copies of this file part
+     * @param this_part part number
+     * @param num_parts number of parts
+     * @param clearport port number of clear communications
+     * @param sslport port number of ssl communications
+     * @param filter the file filter object
+     */
+    private DataPartition(String[] copylocations, int this_part, int num_parts, int clearport, boolean sslport, FileFilter filter,
+            String fileAccessBlob)
+    {
+        this.this_part = this_part;
+        this.num_parts = num_parts;
+        this.rowservicePort = clearport;
+        this.useSSL = sslport;
+        this.fileFilter = filter;
+        this.fileAccessBlob = fileAccessBlob;
+        this.copyLocations = copylocations;
+    }
+
+    /**
+     * Security access blob
+     * @return security access blob
+     */
+    public String getFileAccessBlob()
+    {
+        return this.fileAccessBlob;
+    }
+
+    /**
+     * File part copy locations
+     * @return copy locations
+     */
+    public String[] getCopyLocations()
+    {
+        return this.copyLocations;
+    }
+
+    /**
+     * Location of the ith copy of this file part
+     * @return ip address
+     */
+    public String getCopyIP(int copyindex)
+    {
+        int copiescount = copyLocations.length;
+        if (copyindex < 0 || copyindex >= copiescount) return null;
+
+        return copyLocations[copyindex];
+    }
+
+    /**
+     * Count of copies available for this file part.
+     * @return copy locations size
+     */
+    public int getCopyCount()
+    {
+        return copyLocations.length;
+    }
+
+    /**
+     * Port used for communication in clear.
+     * @return port number
+     */
+    public int getPort()
+    {
+        return rowservicePort;
+    }
+
+    /**
+     * Port used for SSL communication
+     * @return port
+     */
+    public boolean getUseSsl()
+    {
+        return useSSL;
+    }
+
+    /**
+     * File name
+     * @return name
+     */
+    public String getFilename()
+    {
+        return this.file_name;
+    }
+
+    /**
+     * This part's id
+     * @return the id of this part
+     */
+    public int getThisPart()
+    {
+        return this.this_part;
+    }
+
+    /**
+     * Number of parts for this file
+     * @return number of parts
+     */
+    public int getNumParts()
+    {
+        return this.num_parts;
+    }
+
+    /**
+     * The filter object to select specific rows
+     * @return the filter object.
+     */
+    public FileFilter getFilter()
+    {
+        return this.fileFilter;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object
+     */
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.getThisPart());
+        sb.append(" copy locations: {");
+        for (int copyindex = 0; copyindex < getCopyCount(); copyindex++)
+        {
+            if (copyindex > 0) sb.append(", ");
+            sb.append(this.getCopyIP(copyindex));
+        }
+        sb.append("} :");
+        sb.append(this.getPort());
+        return sb.toString();
+    }
+
+    /**
+     * Make an array of data partitions for the supplied HPCC File.
+     * @param fdis the file detail information, multiple if multiple sub-files
+     * @param remap_info remap the IP or ports
+     * @param max_parts the maximum number of partitions or zero for no limit
+     * @return an array of partitions
+     * @throws HpccFileException
+     */
+    public static DataPartition[] createPartitions(DFUFilePartWrapper[] dfupartcopies, ClusterRemapper clusterremapper, int max_parts,
+            String fileAccessBlob) throws HpccFileException
+    {
+        return createPartitions(dfupartcopies, clusterremapper, max_parts, FileFilter.nullFilter(), fileAccessBlob);
+    }
+
+    /**
+     * Comparator function to order file part information.
+     */
+    private static Comparator<DFUFilePartInfo> FilePartInfoComparator = new Comparator<DFUFilePartInfo>() {
+        public int compare(DFUFilePartInfo fpi1, DFUFilePartInfo fpi2)
+        {
+            if (fpi1.getId() < fpi2.getId()) return -1;
+            if (fpi1.getId() > fpi2.getId()) return 1;
+            if (fpi1.getCopy() < fpi2.getCopy()) return -1;
+            if (fpi1.getCopy() > fpi2.getCopy()) return 1;
+            return 0;
+        }
+    };
+
+    public static DataPartition[] createPartitions(DFUFilePartWrapper[] dfuparts, ClusterRemapper clusterremapper, int max_parts, FileFilter filter,
+            String fileAccessBlob) throws HpccFileException
+    {
+        DataPartition[] rslt = new DataPartition[dfuparts.length];
+
+        try
+        {
+            for (int i = 0; i < dfuparts.length; i++)
+            {
+                DataPartition new_dp = new DataPartition(clusterremapper.reviseIPs(dfuparts[i].getCopies()), dfuparts[i].getPartIndex(),
+                        dfuparts.length, clusterremapper.revisePort(null), clusterremapper.getUsesSSLConnection(null), filter, fileAccessBlob);
+                rslt[i] = new_dp;
+            }
+        }
+        catch (Exception e)
+        {
+            throw new HpccFileException("Could not create DataPartition!\n" + e.getMessage());
+        }
+        return rslt;
+    }
+
+    /* (non-Javadoc)
+     * @see org.apache.spark.Partition#index()
+     */
+    public int index()
+    {
+        return this.this_part - 1;
+    }
+}
---
+++ dfsclient/src/main/java/org/hpccsystems/dfs/client/IRecordReader.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *     HPCC SYSTEMS software Copyright (C) 2018 HPCC Systems®.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *******************************************************************************/
+package org.hpccsystems.dfs.client;
+
+import org.hpccsystems.dfs.client.IRecordBuilder;
+
+import org.hpccsystems.commons.errors.HpccFileException;
+
+/**
+ * Interface for the HPCC Systems remote file readers.
+ */
+public interface IRecordReader
+{
+
+    public void initialize(IRecordBuilder recordBuilder) throws Exception;
+
+    /**
+    * Are there more records?  The first time used will trigger a
+    * remote file read.
+    * @return true if there is at least one more record
+    * @throws HpccFileException if there was a failure on the back end.  This
+    * error is not recoverable by a retry.
+    */
+    public boolean hasNext() throws HpccFileException;
+
+    /**
+     * Produce the next record.
+     * @return a record
+     * @throws HpccFileException error on the back end, not recoverable
+     */
+    public Object getNext() throws HpccFileException;
+}
