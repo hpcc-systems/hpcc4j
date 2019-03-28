78aac3d2632a0d96290b6de62abd34e21acecb64
Rodrigo <rodrigo.pastrana@lexisnexis.com>
2019-03-28 11:18:18 -0400

    JAPI-166 Expose all 7.2+ WSDFU create file options (#255)
    - Adds new methods which expose previously hidden options
    - Adds wrapper around wsdfu filetype enum
    
    Signed-off-by: Pastrana <PastraRX@RISK.REGN.NET>

---
+++ wsclient/src/main/java/org/hpccsystems/ws/client/wrappers/wsdfu/DFUFileTypeWrapper.java
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+package org.hpccsystems.ws.client.wrappers.wsdfu;
+
+/**
+ * DFUFileTypeWrapper
+ *
+ * Simple wrapper around WSDFU file type enum generated stub
+ * Provides layer of indirection between caller and ESP version specific logic
+ *
+ */
+public class DFUFileTypeWrapper extends org.hpccsystems.ws.client.gen.wsdfu.v1_50.DFUFileType
+{
+    public static final java.lang.String _Flat = "Flat";
+    public static final java.lang.String _Index = "Index";
+    public static final java.lang.String _Xml = "Xml";
+    public static final java.lang.String _Csv = "Csv";
+    public static final DFUFileTypeWrapper Flat = new DFUFileTypeWrapper(_Flat);
+    public static final DFUFileTypeWrapper Index = new DFUFileTypeWrapper(_Index);
+    public static final DFUFileTypeWrapper Xml = new DFUFileTypeWrapper(_Xml);
+    public static final DFUFileTypeWrapper Csv = new DFUFileTypeWrapper(_Csv);
+
+	private static final long serialVersionUID = 1L;
+
+	protected DFUFileTypeWrapper(String value)
+	{
+		super(value);
+	}
+}
