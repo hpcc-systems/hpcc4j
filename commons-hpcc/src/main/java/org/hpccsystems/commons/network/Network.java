0eb3dad7b1dfc5490ba776f3cc9a62151f41d8c1
Rodrigo <rodrigo.pastrana@lexisnexis.com>
2019-04-17 10:40:01 -0400

    JAPI-175 Adds local address support to commons-hpcc (#270)
    - Adds ipv4/6 islocaladdress methods
    - Provides list of all local (non-loopback addresses)
    - Provides JUNIT tests
    
    Signed-off-by: Pastrana, Rodrigo <rodrigo.pastrana@lexisnexis.com>

---
+++ commons-hpcc/src/main/java/org/hpccsystems/commons/network/Network.java
@@ -0,0 +1,123 @@
+package org.hpccsystems.commons.network;
+
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+import java.util.HashMap;
+
+import org.apache.log4j.*;
+
+public class Network
+{
+    private static final Logger log = Logger.getLogger(Network.class.getName());
+
+    /**
+     * Provides non-loopback address bound to given NIC name
+     * @param interfacename  - The NIC name to draw the local address from, or '*'
+     * @return               - Local ipv4 address
+     * @throws Exception
+     */
+    public static String getLocalAddress(String interfacename) throws Exception
+    {
+        return getLocalAddress(interfacename, false);
+    }
+
+    /**
+     * Provides non-loopback address bound to given NIC name
+     *
+     * @param interfacename - The NIC name to draw the local address from, or '*'
+     * @param ipv6          - True if ipv6 desired, False if ipv4 desired
+     * @return
+     * @throws Exception
+     */
+    public static String getLocalAddress(String interfacename, boolean ipv6) throws Exception
+    {
+        if (interfacename == null || interfacename.isEmpty())
+            throw new Exception("getLocalAddress: Must provide valid NIC name, or '*'");
+
+        if (interfacename.trim().equals("*"))
+        {
+            Enumeration<NetworkInterface> nics;
+            try
+            {
+                nics = NetworkInterface.getNetworkInterfaces();
+                while ( nics.hasMoreElements() )
+                {
+                    NetworkInterface nic = nics.nextElement();
+                    Enumeration<InetAddress> addresses = nic.getInetAddresses();
+                    while ( addresses.hasMoreElements() )
+                    {
+                        InetAddress addr = addresses.nextElement();
+                        if (!addr.isLoopbackAddress())
+                        {
+                            if ((addr instanceof Inet4Address && !ipv6) || (addr instanceof Inet6Address && ipv6))
+                                return addr.getHostAddress();
+                        }
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                throw new Exception("getLocalAddress: Failed to retrieve local network address information", e);
+            }
+        }
+        else
+        {
+            NetworkInterface nic = NetworkInterface.getByName(interfacename);
+            Enumeration<InetAddress> addresses = nic.getInetAddresses();
+
+            while ( addresses.hasMoreElements() )
+            {
+                InetAddress addr = addresses.nextElement();
+                if ((addr instanceof Inet4Address && !ipv6) || (addr instanceof Inet6Address && ipv6))
+                    return addr.getHostAddress();
+            }
+        }
+
+        throw new Exception("getLocalAddress: Could not find local address");
+    }
+
+    /**
+     * Determines if address provided is considered a local address
+     * Uses cached list of all non-loopback addresses bound to all local NICs
+     *
+     *
+     * @param address ipv4/6 to be compared to all known local addresses
+     *
+     * @return True if address is part of known local addresses False otherwise
+     */
+    public static boolean isLocalAddress(String address)
+    {
+          return LocalAddresses.containsKey(address.toUpperCase());
+    }
+
+    private static HashMap<String, InetAddress> LocalAddresses = new HashMap<String, InetAddress>();
+    static
+    {
+        Enumeration<NetworkInterface> ifaces;
+        try
+        {
+            ifaces = NetworkInterface.getNetworkInterfaces();
+            while ( ifaces.hasMoreElements() )
+            {
+                NetworkInterface iface = ifaces.nextElement();
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+
+                while ( addresses.hasMoreElements() )
+                {
+                    InetAddress addr = addresses.nextElement();
+                    if (!addr.isLoopbackAddress())
+                        LocalAddresses.put(addr.getHostAddress().toUpperCase(), addr);
+                }
+            }
+        }
+        catch (SocketException e)
+        {
+            log.error("Failed to retrieve local network address information");
+            log.error(e);
+        }
+    }
+}
