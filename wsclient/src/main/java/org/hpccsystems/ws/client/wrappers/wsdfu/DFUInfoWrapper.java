5ebd9eb9a66b7b80b52aade476fa4d72730602f1
drealeed <drealeed2@yahoo.com>
2019-05-21 15:41:24 -0400

    new dfu info wrapper class

---
+++ wsclient/src/main/java/org/hpccsystems/ws/client/wrappers/wsdfu/DFUInfoWrapper.java
@@ -0,0 +1,72 @@
+/*##############################################################################
+
+    HPCC SYSTEMS software Copyright (C) 2018 HPCC Systems®.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+############################################################################## */
+
+package org.hpccsystems.ws.client.wrappers.wsdfu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hpccsystems.ws.client.gen.wsdfu.v1_51.DFUInfoResponse;
+import org.hpccsystems.ws.client.wrappers.WUExceptionWrapper;
+
+public class DFUInfoWrapper
+{
+    private DFUFileDetailWrapper detailInfo;
+    private List<WUExceptionWrapper> exceptions = new ArrayList<WUExceptionWrapper>();
+
+    public DFUInfoWrapper (DFUInfoResponse resp) throws Exception
+    {
+        this.detailInfo=new DFUFileDetailWrapper(resp.getFileDetail());
+        if (resp.getExceptions() != null && resp.getExceptions().getException()!= null) {
+            for (int i=0; i < resp.getExceptions().getException().length;i++) {
+                this.exceptions.add(new WUExceptionWrapper(resp.getExceptions().getException()[i]));
+            }
+        }
+    }
+
+    /**
+     * @return the detailInfo
+     */
+    public DFUFileDetailWrapper getFileDetail()
+    {
+        return detailInfo;
+    }
+
+    /**
+     * @param detailInfo the detailInfo to set
+     */
+    public void setFileDetail(DFUFileDetailWrapper detailInfo)
+    {
+        this.detailInfo = detailInfo;
+    }
+
+    /**
+     * @return the exceptions
+     */
+    public List<WUExceptionWrapper> getExceptions()
+    {
+        return exceptions;
+    }
+
+    /**
+     * @param exceptions the exceptions to set
+     */
+    public void setExceptions(List<WUExceptionWrapper> exceptions)
+    {
+        this.exceptions = exceptions;
+    }
+}
