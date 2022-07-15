5253d67e3e4847a0a21fbf0435e2ce40fd38791b
Rodrigo <rodrigo.pastrana@lexisnexis.com>
2022-07-15 17:23:56 -0400

    HPCC4J-471 Upgrade to ANTLR 4.10.1 (#576)
    HPCC4J-471 Upgrade to ANTLR 4.10.1
    
    - Bumps ANTLR dep to 4.10.1
    - Postpends gen on generated parser package
    - Suppresses automatic re-build of antlr parser
    - Includes generated antlr parser
    
    Signed-off-by: Rodrigo Pastrana <rodrigo.pastrana@lexisnexisrisk.com>

--- wsclient/src/main/java/org/hpccsystems/ws/client/antlr/package-info.java
+++ wsclient/src/main/java/org/hpccsystems/ws/client/antlr/package-info.java
@@ -1,5 +1,8 @@
 /**
  * Used internally to parse various formats of ECL record definitions.
  *
+ * Pre-Generated EclRecordReader ANTLR parser is provided.
+ * Can be customized and re-generated via mvn profile "generate-antlr-eclrecparser"
  */
+
 package org.hpccsystems.ws.client.antlr;
