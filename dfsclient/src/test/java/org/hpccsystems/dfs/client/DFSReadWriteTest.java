3d47b987a8d58fe3f684bdc8f5d7b3b74906bb40
LeedDX <drea.leed@reedelsevier.com>
2024-05-16 07:49:36 -0400

    jira-595: revert test changes

--- dfsclient/src/test/java/org/hpccsystems/dfs/client/DFSReadWriteTest.java
+++ dfsclient/src/test/java/org/hpccsystems/dfs/client/DFSReadWriteTest.java
@@ -65,8 +65,12 @@ import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DFSReadWriteTest extends BaseRemoteTest
 {
-    private static final String[] datasets       = { "~benchmark::integer::20kb", "~benchmark::all_types::200kb"};//, "~unit_test::all_types::xml", "~unit_test::all_types::json", "~unit_test::all_types::csv" };
-    private static final int[]    expectedCounts = { 1250, 5600 };//, 10000, 10000, 10000, 10000};
+    private static final String[] datasets       = { "~benchmark::integer::20kb", "~unit_test::all_types::thor", "~unit_test::all_types::xml", "~unit_test::all_types::json", "~unit_test::all_types::csv" };
+    private static final int[]    expectedCounts = { 1250, 5600, 10000, 10000, 10000, 10000};
+
+    //use until standard test is working
+    //        private static final String[] datasets       = { "~benchmark::integer::20kb", "~benchmark::all_types::200kb"};//, "~unit_test::all_types::xml", "~unit_test::all_types::json", "~unit_test::all_types::csv" };
+    //        private static final int[]    expectedCounts = { 1250, 5600 };//, 10000, 10000, 10000, 10000};
     private static final Version newProtocolVersion = new Version(8,12,10);
 
 
@@ -141,7 +145,7 @@ public class DFSReadWriteTest extends BaseRemoteTest
             writeFile(records, fileName, recordDef, connTO);
 
             HPCCFile file = new HPCCFile(fileName, connString , hpccUser, hpccPass);
-            List<HPCCRecord> readRecords = readFile(file, 100000, false, false, BinaryRecordReader.TRIM_STRINGS);
+            List<HPCCRecord> readRecords = readFile(file, 10000, false, false, BinaryRecordReader.TRIM_STRINGS);
 
             for (int i = 0; i < records.size(); i++)
             {
@@ -188,7 +192,7 @@ public class DFSReadWriteTest extends BaseRemoteTest
             writeFile(records, fileName, recordDef, connTO);
 
             HPCCFile file = new HPCCFile(fileName, connString , hpccUser, hpccPass);
-            List<HPCCRecord> readRecords = readFile(file, 100000, false, false, BinaryRecordReader.TRIM_STRINGS);
+            List<HPCCRecord> readRecords = readFile(file, 10000, false, false, BinaryRecordReader.TRIM_STRINGS);
 
             for (int i = 0; i < records.size(); i++)
             {
