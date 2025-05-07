253f2ee51c7a56f1cb494f8c925b1d98b7cf1d71
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexisrisk.com>
2025-05-07 14:17:24 -0400

    HPCC4J-694 Update Axis2 version to 2.0
    - Declare new http client version required by axis2 2.o
    - Recreate generated code
    
    Signed-off-by: Rodrigo Pastrana <rodrigo.pastrana@lexisnexisrisk.com>
    
    #rkunits/WrapperReport.txt

---
+++ wsclient/src/main/java/org/hpccsystems/ws/client/gen/axis2/wsworkunits/latest/EspSoapFault.java
@@ -0,0 +1,39 @@
+/**
+ * EspSoapFault.java
+ *
+ * <p>This file was auto-generated from WSDL by the Apache Axis2 version: 2.0.0 Built on : Mar 05,
+ * 2025 (12:49:45 HST)
+ */
+package org.hpccsystems.ws.client.gen.axis2.wsworkunits.latest;
+
+public class EspSoapFault extends java.lang.Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  private org.hpccsystems.ws.client.gen.axis2.wsworkunits.latest.Exceptions faultMessage;
+
+  public EspSoapFault() {
+    super("EspSoapFault");
+  }
+
+  public EspSoapFault(java.lang.String s) {
+    super(s);
+  }
+
+  public EspSoapFault(java.lang.String s, java.lang.Throwable ex) {
+    super(s, ex);
+  }
+
+  public EspSoapFault(java.lang.Throwable cause) {
+    super(cause);
+  }
+
+  public void setFaultMessage(
+      org.hpccsystems.ws.client.gen.axis2.wsworkunits.latest.Exceptions msg) {
+    faultMessage = msg;
+  }
+
+  public org.hpccsystems.ws.client.gen.axis2.wsworkunits.latest.Exceptions getFaultMessage() {
+    return faultMessage;
+  }
+}
